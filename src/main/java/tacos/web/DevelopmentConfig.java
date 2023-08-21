dbd7c4f0ca07d3bdd73775a03d256cae8fea34a8
Дмитрий Данилов <danilov0330@gmail.com>
2023-08-21 22:03:40 +0300

    before_disaster

--- src/main/java/tacos/web/DevelopmentConfig.java
+++ src/main/java/tacos/web/DevelopmentConfig.java
@@ -1,5 +1,7 @@
 package tacos.web;
 
+import java.util.Arrays;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,9 +9,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import tacos.data.IngredientRepository;
+import tacos.data.TacoRepository;
 import tacos.data.UserRepository;
 import tacos.model.Ingredient;
 import tacos.model.Ingredient.Type;
+import tacos.model.Taco;
 import tacos.model.User;
 
 @Profile("!prod")
@@ -17,29 +21,52 @@ import tacos.model.User;
 public class DevelopmentConfig {
 
     @Bean
-    public CommandLineRunner dataLoader(IngredientRepository ingredientRepository, UserRepository userRepository,
-            PasswordEncoder passwordEncoder) {
-        return new CommandLineRunner() {
-            @Override
-            public void run(String... args) throws Exception {
-                ingredientRepository.deleteAll();
-                userRepository.deleteAll();
+    public CommandLineRunner dataLoader(
+            IngredientRepository ingredientRepository,
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            TacoRepository tacoRepository) {
+        return args -> {
+            Ingredient flourTortilla = new Ingredient("FLTO", "Flour Tortilla", Type.WRAP);
+            Ingredient cornTortilla = new Ingredient("COTO", "Corn Tortilla", Type.WRAP);
+            Ingredient groundBeef = new Ingredient("GRBF", "Ground Beef", Type.PROTEIN);
+            Ingredient carnitas = new Ingredient("CARN", "Carnitas", Type.PROTEIN);
+            Ingredient tomatoes = new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES);
+            Ingredient lettuce = new Ingredient("LETC", "Lettuce", Type.VEGGIES);
+            Ingredient cheddar = new Ingredient("CHED", "Cheddar", Type.CHEESE);
+            Ingredient jack = new Ingredient("JACK", "Monterrey Jack", Type.CHEESE);
+            Ingredient salsa = new Ingredient("SLSA", "Salsa", Type.SAUCE);
+            Ingredient sourCream = new Ingredient("SRCR", "Sour Cream", Type.SAUCE);
 
-                ingredientRepository.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
-                ingredientRepository.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
-                ingredientRepository.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
-                ingredientRepository.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
-                ingredientRepository.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
-                ingredientRepository.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
-                ingredientRepository.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
-                ingredientRepository.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
-                ingredientRepository.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
-                ingredientRepository.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+            ingredientRepository.save(flourTortilla);
+            ingredientRepository.save(cornTortilla);
+            ingredientRepository.save(groundBeef);
+            ingredientRepository.save(carnitas);
+            ingredientRepository.save(tomatoes);
+            ingredientRepository.save(lettuce);
+            ingredientRepository.save(cheddar);
+            ingredientRepository.save(jack);
+            ingredientRepository.save(salsa);
+            ingredientRepository.save(sourCream);
 
-                userRepository.save(new User("habuma", passwordEncoder.encode("password"),
-                        "Craig Walls", "123 North Street", "Cross Roads", "TX",
-                        "76227", "123-123-1234"));
-            }
+            Taco taco1 = new Taco();
+            taco1.setName("Carnivore");
+            taco1.setIngredients(Arrays.asList(flourTortilla, groundBeef, carnitas, sourCream, salsa, cheddar));
+            tacoRepository.save(taco1);
+
+            Taco taco2 = new Taco();
+            taco2.setName("Bovine Bounty");
+            taco2.setIngredients(Arrays.asList(cornTortilla, groundBeef, cheddar, jack, sourCream));
+            tacoRepository.save(taco2);
+
+            Taco taco3 = new Taco();
+            taco3.setName("Veg-Out");
+            taco3.setIngredients(Arrays.asList(flourTortilla, cornTortilla, tomatoes, lettuce, salsa));
+            tacoRepository.save(taco3);
+
+            userRepository.save(new User("dima", passwordEncoder.encode("123456789"),
+                    "Dima Danilov", "Ostrovitanova", "Moscow", "Moscow",
+                    "117279", "8-999-999-99-99"));
         };
     }
 }
